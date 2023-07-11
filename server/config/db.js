0641f8b59ffa3adc8524bad55ef7b96547feddde
eugeniabut <eugeniabutkevich2022@gmail.com>
2023-07-11 16:12:10 +0200

    createUser modified with email

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+   
     
     const MONGO_URI = process.env.CONNECTION_URL
 
     try{
         await mongoose.connect(MONGO_URI)
         console.log("DB connected")
+       
     }
     catch(err){
         console.log(err.message)
