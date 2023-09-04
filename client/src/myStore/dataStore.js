144084e4c47947b70369920adf6cd846dedd50b3
Meriem Khadraoui <miyakhad@gmail.com>
2023-09-04 10:07:01 +0200

    new add

--- client/src/myStore/dataStore.js
+++ client/src/myStore/dataStore.js
@@ -8,11 +8,11 @@ newsData:[],
 error:false,
 getData:async()=>{
   
-    const options1 = {
-      method: 'GET',
-      url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=333ad23a145a4832b2ca734696e3b06b',
-      //url:"https://newsdata.io/api/1/crypto?apikey=pub_260065beaa4df95c2f842200733d53e9f2c3b"
-    };
+    // const options1 = {
+    //   method: 'GET',
+    //   url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=333ad23a145a4832b2ca734696e3b06b',
+    //   //url:"https://newsdata.io/api/1/crypto?apikey=pub_260065beaa4df95c2f842200733d53e9f2c3b"
+    // };
     const options2 = {
       method: 'GET',
       url:'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
@@ -20,13 +20,16 @@ getData:async()=>{
 
     try {
       
-        const newsData = await axios.request(options1);
+        // const newsData = await axios.request(options1);
         const cryptos = await axios.request(options2);
         // console.log(cryptos);
-        set({cryptos:cryptos.data, newsData:newsData.data})
+        set({cryptos:cryptos.data, 
+          // newsData:"newsData.data"
+        })
     } catch (error) {
 set({error:error.message})  
   }
   },
+  
   
 }))
